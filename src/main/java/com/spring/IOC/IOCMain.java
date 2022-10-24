ddabaaeb26dcff73d40916f036b8ae89f4168a4d
hasaki1025 <wdnmdwgzdl666@163.com>
2022-10-24 12:22:29 +0800

    spring-source-test

--- src/main/java/com/spring/IOC/IOCMain.java
+++ src/main/java/com/spring/IOC/IOCMain.java
@@ -4,10 +4,13 @@ package com.spring.IOC;
 import com.spring.IOC.Config.MyConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.lang.annotation.Annotation;
+
 public class IOCMain {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
         System.out.println();
         for (String beanDefinitionName : context.getBeanDefinitionNames()) {
             System.out.println(beanDefinitionName);
